07d99773aaf259d4d7aed4194ba88712a954d43a
David Zhang <dz@davids-air.speedport_w_724v_09011603_00_023>
2016-06-13 00:47:15 +0200

    py

---
+++ pythonutils/parse_and_split.py
@@ -0,0 +1,76 @@
+import gzip
+import pandas as pd
+from collections import Counter
+import string
+import spacy
+import numpy as np
+import array
+import itertools
+from spacy import attrs
+
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield eval(l)
+
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')    
+    
+def get_rev(path, index):
+    for d in parse(path):
+        yield unicode(d[index], 'utf-8')
+
+def exclude(word):
+    if word.isdigit() or word.isspace() or word in string.punctuation:
+        return True
+    else:
+        return False
+
+def get_frequent_words(reviews, threshold, nlp):
+    counter = Counter()
+    doc = nlp.pipe(reviews, n_threads=4, entity=False, parse=False)
+    for review in doc:
+        counter.update(review.count_by(spacy.attrs.LEMMA))
+        
+    fc = {key:val for key, val in counter.iteritems() if not exclude(nlp.vocab.strings[key])}
+    #fc = {key:val for key, val in counter.iteritems() if not exclude(nlp.vocab.strings[key])}
+    
+    return [key for key, val in fc.iteritems() if val > threshold]
+
+def get_word_bags(voc, reviews, nlp):
+    ret = []
+    voc_pos = {word:ind for ind, word in enumerate(voc)}
+    doc = nlp.pipe(reviews, n_threads=4, entity=False, parse=False)
+    for review in doc:
+        #yield review.count_by(spacy.attrs.LEMMA).keys(), review.count_by(spacy.attrs.LEMMA).values()
+        ret.append({voc_pos[word]:count for word, count in review.count_by(spacy.attrs.LEMMA).iteritems() \
+                    if word in voc_pos})
+    return ret
+
+def get_k_splits(df, index, uni, k):
+    length = len(uni)
+    k_len = [length/k] * (k-1)+[length - length/k * (k-1)]
+    k_splits = np.array(k_len).cumsum() - 1
+    
+    yield 0, df[df[index] <= uni[k_splits[0]]]
+    for i in range(1,k):
+        yield k_splits[i-1]+1, df[(uni[k_splits[i-1]] < df[index]) & (df[index] <= uni[k_splits[i]])]
+
+def split_dict(d, k_splits):
+    ret = [{key: val for key, val in d.iteritems() if (key <= k_splits[0])}]
+    for i in range(1,len(k_splits)):
+        ret += [{key: val for key, val in d.iteritems() if (k_splits[i-1] < key) & (key <= k_splits[i])}]
+    return ret
+
+def get_k_splits_words(df, wbdf, words, k):
+    length = len(words)
+    k_len = [length/k] * (k-1)+[length - length/k * (k-1)]
+    k_splits = np.array(k_len).cumsum() - 1
+    offsets = [0] + (k_splits[:len(k_splits)-1]+1).tolist()
+    return offsets, df.join(wbdf.apply(lambda x: pd.Series(split_dict(x, k_splits))))
+
---
+++ pythonutils/save_tensor.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import os
+import string
+import spacy
+import numpy as np
+import array
+import itertools
+import json
+import argparse
+import time
+from parse_and_split import *
+
+def save(f, offset, rows, cols, bags, words, vals):
+    assert len(rows) == len(cols) == len(bags), "Wrong list length for save"
+    print offset, max(rows)+1, max(cols)+1, max(words)+1, len(rows), len(vals)
+    array.array("I", [offset, max(rows)+1, max(cols)+1, max(words)+1, len(rows), len(vals)]).write(f)
+    array.array("I", rows).write(f)
+    array.array("I", cols).write(f)
+    array.array("I", bags).write(f)
+    array.array("I", words).write(f)
+    array.array("f", vals).write(f)
+
+
+def save_df(df, path, userkeys, prodkeys, offset):
+    sorteddf = df.sort_values(['reviewerID', 'asin'])
+    users = [userkeys[u] for u in sorteddf['reviewerID']]
+    prods = [prodkeys[p] for p in sorteddf['asin']]
+    bags = np.array([len(wb) for wb in sorteddf['wordBags']]).cumsum() # maybe -1?
+    wbtmp = [sorted([(pos, count) for pos, count in wb.iteritems()], key=lambda x: x[0]) for wb in sorteddf['wordBags']]
+    words, values = zip(*list(itertools.chain.from_iterable(wbtmp)))
+
+    with open(path, 'wb') as f:
+        save(f, offset, users, prods, bags, words, values)
+
+
+def save_df_word(df, path, userkeys, prodkeys, offset, k):
+    sorteddf = df.sort_values(['reviewerID', 'asin'])
+    users = [userkeys[u] for u in sorteddf['reviewerID']]
+    prods = [prodkeys[p] for p in sorteddf['asin']]
+    for ind in range(k):
+        bags = np.array([len(wb) for wb in sorteddf[ind]]).cumsum() # maybe -1?
+        wbtmp = [sorted([(pos, count) for pos, count in wb.iteritems()], key=lambda x: x[0]) for wb in sorteddf[ind]]
+        words, values = zip(*list(itertools.chain.from_iterable(wbtmp)))
+        with open(path + `ind`, 'wb') as f:
+            save(f, offset[ind], users, prods, bags, words, values)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", type=float, default=0.2, help="Test size")
+    parser.add_argument("k", type=int, help="Split k-ways")
+    parser.add_argument("data", help="Review data file")
+    parser.add_argument("out", help="Output directory")
+
+    args = parser.parse_args()
+
+    start = time.clock()
+
+    # build vocab
+    eng = spacy.en.English()
+    end = time.clock()
+    print "eng", start - end
+
+    start = time.clock()
+
+    words = get_frequent_words(get_rev(args.data, 'reviewText'), 10, eng)
+
+    end = time.clock()
+    print "build vocab done", start - end
+
+    start = time.clock()
+
+    # get word frequency per review
+    bags = get_word_bags(words, get_rev(args.data, 'reviewText'), eng)
+
+    end = time.clock()
+    print "word bags done", start - end
+    # parse dataframe
+    df = getDF(args.data)
+
+    # delete useless stuff
+    df.drop(['reviewText', 'reviewerName', 'helpful', 'unixReviewTime', 'overall', 'reviewTime', 'summary'], axis=1, inplace=True)
+
+    # put bags into pd df
+    wbdf = pd.DataFrame({'wordBags':bags})
+
+    # unique lists
+    users = df['reviewerID'].sort_values().unique()
+    prods = df['asin'].sort_values().unique()
+
+    userkeys = {u: i for i, u in enumerate(users)}
+    prodkeys = {p: i for i, p in enumerate(prods)}
+
+    # df used to split in user and prod dimensions
+    df_total = pd.concat([df,wbdf], axis=1, join='inner', copy='false')
+
+    # save user-split train files
+    for ind, (offset, df_user) in enumerate(get_k_splits(df_total, 'reviewerID', users, args.k)):
+        outpath = os.path.join(args.out, "_user_train" + `ind`)
+        save_df(df_user, outpath, userkeys, prodkeys, offset)
+    print "saved usersplit"
+    # save product-split train files
+    for ind, (offset, df_prod) in enumerate(get_k_splits(df_total, 'asin', prods, args.k)):
+        outpath = os.path.join(args.out, "_prod_train" + `ind`)
+        save_df(df_prod, outpath, userkeys, prodkeys, offset)
+    print "saved prodsplit"
+    # save words-split train files
+    offset, df_word = get_k_splits_words(df_total, df_total['wordBags'], words, args.k)
+    print df_word.columns.values
+    outpath = os.path.join(args.out, "_word_train")
+    save_df_word(df_word, outpath, userkeys, prodkeys, offset, args.k)
+    print "saved wordsplit"
+
+    json.dump({'users': len(users), 'products': len(prods), 'words' : len(words), 'vocab' : [eng.vocab.strings[w] for w in words]},
+        open(os.path.join(args.out, "meta.txt"), "w"))
+
+if __name__ == "__main__":
+    main()
